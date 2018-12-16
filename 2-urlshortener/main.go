425536afc65036bc9a6b58fc0d9d4ff7b2e4cc99
Edwin Hoksberg <mail@edwinhoksberg.nl>
2018-12-16 22:03:50 +0100

    Add completed exercise #2

---
+++ 2-urlshortener/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"./urlshortener"
+)
+
+var yamlFilePath string
+var jsonFilePath string
+
+func init() {
+	// Parse command line arguments
+	flag.StringVar(&yamlFilePath, "yaml", "paths.yml", "a file that contains paths in yaml format")
+	flag.StringVar(&jsonFilePath, "json", "paths.json", "a file that contains paths in json format")
+
+	flag.Parse()
+}
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshortener.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yamlFileHandler, err := urlshortener.YAMLFileHandler(yamlFilePath, mapHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Build the JSONFileHandler using the YAMLHandler as the fallback
+	jsonFileHandler, err := urlshortener.JSONFileHandler(jsonFilePath, yamlFileHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Starting the server on :8080")
+
+	// Start listening...
+	http.ListenAndServe(":8080", jsonFileHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Create a landing page
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!")
+	})
+
+	return mux
+}
---
+++ 2-urlshortener/urlshortener/handler.go
@@ -0,0 +1,87 @@
+package urlshortener
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		redirectUrl, exists := pathsToUrls[r.URL.Path]
+
+		if exists {
+			log.Printf("Redirecting '%s' to '%s'...", r.URL.Path, redirectUrl)
+
+			http.Redirect(w, r, redirectUrl, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+func YAMLFileHandler(yamlFilePath string, fallback http.Handler) (http.HandlerFunc, error) {
+	// Open the yaml file containing paths and urls
+	yamlFile, err := os.Open(yamlFilePath)
+	if err != nil {
+		return nil, err
+	}
+	// Defer closing the file after it has been read completely
+	defer yamlFile.Close()
+
+	// Read the entire file
+	yml, err := ioutil.ReadAll(yamlFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the file contents into a golang map
+	var parsedYaml []map[string]string
+	if err := yaml.Unmarshal(yml, &parsedYaml); err != nil {
+		return nil, err
+	}
+
+	// Process the map to something that the MapHandler can process
+	pathsToUrls := make(map[string]string)
+	for _, value := range parsedYaml {
+		key := value["path"]
+		pathsToUrls[key] = value["url"]
+	}
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func JSONFileHandler(jsonFilePath string, fallback http.Handler) (http.HandlerFunc, error) {
+	// Open the json file containing paths and urls
+	jsonFile, err := os.Open(jsonFilePath)
+	if err != nil {
+		return nil, err
+	}
+	// Defer closing the file after it has been read completely
+	defer jsonFile.Close()
+
+	// Read the entire file
+	jsn, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the file contents into a golang map
+	var parsedYaml []map[string]string
+	if err := json.Unmarshal(jsn, &parsedYaml); err != nil {
+		return nil, err
+	}
+
+	// Process the map to something that the MapHandler can process
+	pathsToUrls := make(map[string]string)
+	for _, value := range parsedYaml {
+		key := value["path"]
+		pathsToUrls[key] = value["url"]
+	}
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
